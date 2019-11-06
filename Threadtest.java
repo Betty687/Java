a1a9bceec77c807ab983ef5e37d6ea5dab25322b
Betty687 <54886922+Betty687@users.noreply.github.com>
2019-11-06 17:59:51 +0800

    setpriority

---
+++ Threadtest.java
@@ -0,0 +1,43 @@
+package test2;
+
+public class Threadtest {
+	static Lefthand left;
+		static Righthand right;
+		public static void main(String[] args)
+		{	left = new Lefthand();
+			right = new Righthand();
+			left.setPriority(Thread.MAX_PRIORITY);
+			left.start();
+			try {Thread.sleep(100);
+}
+			catch(Exception e) {}
+			right.start();
+		}
+}
+class Lefthand extends Thread
+{	
+	public void run()
+	{
+		for(int i=0;i<=5;i++)
+		{	System.out.println("You are Students!");
+			try
+			{	sleep(500);
+			}catch(Exception e){}
+		}
+	
+   }
+}
+class Righthand extends Thread
+{	public void run()
+	{	for(int i=0;i<=5;i++)
+		{	System.out.println("I am a Teacher!");
+			try
+			{	sleep(500);
+			}
+			catch(Exception e){}
+		}
+	}
+
+
+}
+
